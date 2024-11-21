efc5d23b4e85f520be5356fc239fe48d5356882b
sebocam <cambalsebo@gmail.com>
2024-11-21 16:06:23 +0100

    Lore button in menu

--- Assets/Scripts/UI/MainMenu/ButtonHandler.cs
+++ Assets/Scripts/UI/MainMenu/ButtonHandler.cs
@@ -9,4 +9,15 @@ public class ButtonHandler : MonoBehaviour
     {
         SceneManager.LoadScene("Gameplay");
     }
+
+    public void exitButton()
+    {
+        Application.Quit();
+    }
+
+    public void loreButton()
+    {
+        // TODO: Add lore scene
+        // SceneManager.LoadScene("Lore");
+    }
 }
