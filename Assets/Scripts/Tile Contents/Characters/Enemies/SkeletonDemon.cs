97ab35255d11d88ec9e1955a618f2aa4ea162447
Peter Debnár <peterdebnar8@gmail.com>
2024-12-09 04:10:46 +0100

    Added new enemy SkeletonDemon

---
+++ Assets/Scripts/Tile Contents/Characters/Enemies/SkeletonDemon.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class SkeletonDemon : Enemy
+{
+    protected float randomMoveChance = 0.5f;
+    private int moveTick;
+    private int direction;
+
+    // Start is called before the first frame update
+
+    public override void collideWithCharacter(Character character)
+    {
+        if (character is Player)
+        {
+            character.takeDamage(getScaledDamage(1));
+        }
+    }
+    protected override void Start()
+    {
+        base.Start();
+        direction = UnityEngine.Random.Range(0, 4);
+        moveTick = UnityEngine.Random.Range(0, 5);
+    }
+
+    public override int getBaseMaxHp()
+    {
+        return 6;
+    }
+
+    protected override int getPlayerDetectionRadius()
+    {
+        return 1;
+    }
+
+    protected override void onTick()
+    {
+
+        if (moveTick == 3)
+        {
+            move((Movement)direction, 2);
+            direction = UnityEngine.Random.value > 0.5 ? direction + 1 : direction + 3;
+            direction = direction % 4;
+        }
+        else if (moveTick == 4) 
+        {
+            move((Movement)direction, 1);
+            direction = UnityEngine.Random.Range(0, 4);
+        }
+
+        moveTick = (moveTick + 1) % 5;
+        
+    }
+}
